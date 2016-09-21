6a3bf07e2f146e315dd2e7feaf229c83f23a2be7
John Andersen <johnandersen185@gmail.com>
2016-09-21 15:36:44 -0400

    Added gheadlines and link_get

---
+++ lib/gheadlines.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from pprint import pprint as pp
+
+import requests, sys
+from bs4 import BeautifulSoup as bs
+
+def findurl(gquery):
+    return gquery[gquery.find("https://"):gquery.find("&sa=")]
+
+def filterSrch(rslt):
+    return rslt[0] != "Cached" and rslt[0] and "/url" in rslt[1]
+
+def get(site, topic):
+    headers = {'User-Agent' : 'Mozilla/5.0'}
+    req = requests.get("https://www.google.com/search?q=site:" + \
+            site + "+" + topic + "&tbs=qdr:y", headers=headers)
+    return req.text if req.status_code == 200 else req.status_code
+
+def get5(html):
+    html = bs(html, "html.parser")
+    headlines = [ (tag.text, tag.get('href')) for tag in \
+                                            html.find_all('a')]
+    headlines = list(filter( filterSrch, headlines))
+
+    headlines = [(x[0], findurl(x[1])) for x in headlines]
+    return headlines[:5]
+
+def main():
+    sitename, topic = (sys.argv[1], sys.argv[2])
+    request = get(sitename, topic)
+    if not isinstance(request, int):
+        pp(get5(request))
+    else:
+        print("Your google request failed with {0}".format(request))
+
+if __name__ == "__main__":
+    main()
+
---
+++ lib/link_get.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+#TODO argparser
+
+import requests, sys, re, time
+from bs4 import BeautifulSoup as bs
+
+#FIXME the positional parameters are stupid and useless right now
+def retrieve_links(link, tag_all='search-text', href='a'):
+    '''
+        Summary:
+            Retrieve the links on a given page
+
+        ----------------------------------------------------------------
+        Parameters:
+            link :: The address at which to get the hrefs
+
+             tag_all='search-text' :: The tag under which the hrefs will be found
+
+             href='a' :: The class associated with the href
+
+        ----------------------------------------------------------------
+        Return Values:
+            links :: A list of href links from the given link
+
+    '''
+    page = bs(requests.get(link).text, 'html.parser')
+    links = [] # type: list[str]
+    for tag in page.find_all( href):
+        links.append(tag.get('href'))
+    return links
+
+def print_q_string(links):
+    '''
+        Summary:
+            Print the query_string
+
+        ----------------------------------------------------------------
+        Parameters:
+            links :: The links that need to be made into a query
+
+        ----------------------------------------------------------------
+        Return Values:
+            links :: Returns the links in the query?
+
+    '''
+    end = re.compile('.*(.com)')
+    print('('+'|'.join([end.split(i)[2][:-1] for i in links])+')')
+
+def iterlink(args):
+    '''
+        Summary:
+            iterate over the links to retrieve new links etc. etc.
+
+        ----------------------------------------------------------------
+        Parameters:
+            args :: optional arguments for the iteration (see code for dets)
+
+        ----------------------------------------------------------------
+        Return Values:
+            links :: Returns all of the links from the iterations
+
+    '''
+    links = [] # type: list[str]
+    for i in args:
+        links += retrieve_links(i)
+        time.sleep(2)
+    return links
+
+def main():
+    args = sys.argv[1:]
+    if args:
+        if args[0] == '-a':
+            print_q_string(iterlink(args[1:]))
+        else:
+            for i in args:
+                links = retrieve_links(i)
+                print('\n'.join(links))
+                print_q_string(links)
+                time.sleep(5) #Because fucking requests is blocking
+
+if __name__ == "__main__":
+    main()
