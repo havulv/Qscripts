5247d2f6f5e95be43eb6c94f05f9dd4b087b7d03
John Andersen <johnandersen185@gmail.com>
2016-10-10 17:05:37 -0400

    Improved error handling for bad sites, & no dscrp.

--- lib/site_dscrp.py
+++ lib/site_dscrp.py
@@ -3,6 +3,20 @@
 from bs4 import BeautifulSoup as bs
 import requests, sys
 
+def connection_errors(func):
+    def wrap_request(*args, **kwargs):
+        try:
+            func_ret = func(*args, **kwargs)
+        except requests.exceptions.ConnectionError as e:
+            print("ConnectionError -- did you use https on a site that"
+                    " does not support secure http? Try http instead."
+                    "\n\nDetails :: {}".format(e))
+            sys.exit(0)
+        return func_ret
+    return wrap_request
+
+
+@connection_errors
 def html(sitename):
     req = requests.get(sitename)
     return req.text if req.status_code == 200 else req.status_code
@@ -12,7 +26,7 @@ def sift(html):
     dscrp = page.find('meta', {'name' : 'description'})
     if not dscrp:
         dscrp = page.find('meta', {'property' : 'og:description'})
-    return dscrp.get('content')
+    return dscrp.get('content') if dscrp != None else "No Description"
 
 def main():
     try:
