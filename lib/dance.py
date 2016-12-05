0bf1ce136970d40aa384f32674d99aabfc489340
John Andersen <johnandersen185@gmail.com>
2016-12-05 12:43:32 -0500

    Made improvements to dance

--- lib/dance.py
+++ lib/dance.py
@@ -2,54 +2,44 @@
 from os import system,name
 from time import sleep
 from random import randint
-from sys import exit
+import sys
+
+def ctrl_c(func):
+    def wrap(*args, **kwargs):
+        try:
+            ret = func(*args, **kwargs)
+        except KeyboardInterrupt:
+            ret = None
+            print('\r\n')
+            print('\r          /``````\ ')
+            print(' *(#_#)* { ByeBye }')
+            print('          \______/')
+            exit(0)
+        return ret
+    return wrap
 
 def printer(dancers, wait, clear=True):
-    '''
-        Summary:
-            Prints out the dancers!
 
-        ----------------------------------------------------------------
-        Parameters:
-            dancers :: Who do you want to shake their booty?
-
-             wait :: How long should we wait
-
-             clear=True :: Get rid of the beautiful dancers?
-
-        ----------------------------------------------------------------
-        Return Values:
-            VOID
-    '''
-    hands_up = ' `\_(^_^)`\_ '
-    hands_down = ' _/`(^_^)_/` '
-    whats_going_on = '  ~~(*_*)~~  '
-    shrug = ' `\_(^_^)_/` '
-    l_wink = ' _/`(-_^)`\_ '
-    r_wink = ' _/`(^_-)`\_ '
-    dance_moves = [
-            hands_up, hands_down, whats_going_on,
-            shrug, l_wink, r_wink
-            ]
+    dance_moves = [' `\_(^_^)`\_ ', ' _/`(^_^)_/` ', '  ~~(*_*)~~  ',
+            ' `\_(^_^)_/` ', ' _/`(-_^)`\_ ', ' _/`(^_-)`\_ ',
+            ' /.(@_@).\ ']
     if clear:
         system('cls' if name == 'nt' else 'clear')
         print('\n'*15)
     while True:
         print(
-            *[dance_moves[randint(0,5)] for i in range(dancers)],
+            *[dance_moves[randint(0,6)] for i in range(dancers)],
             end='\r', flush=True
             )
         sleep(wait)
 
-def main():
-    try:
-        printer(5, .1)
-    except KeyboardInterrupt:
-        print('\r\n')
-        print('\r          /``````\ ')
-        print(' *(#_#)* { ByeBye }')
-        print('          \______/')
-        exit(0)
+@ctrl_c
+def main(clear=False):
+    clear = bool(clear)
+    printer(5, .1, clear)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        main()
