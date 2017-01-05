9af0df27f7cb4beb9790f5ddac9bb48cf6e0ee58
John Andersen <johnandersen185@gmail.com>
2017-01-05 11:48:22 -0500

    Abstracted out dancers

--- lib/dance.py
+++ lib/dance.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-from os import system,name
+from os import system, name, get_terminal_size
 from time import sleep
 from random import randint
 import sys
+
+def move():
+    eyes = [ "^", "*", "@", "-", "#" ]
+    arms = ["`\_", "_/`", "/.", "\.", "~~"]
+    mouth = ["_", "o"]
+    return "{: >4}({}{}{}){: <4}".format(
+                                arms[randint(0, len(arms)-1)],
+                                eyes[randint(0, len(eyes)-1)],
+                                mouth[randint(0, len(mouth)-1)],
+                                eyes[randint(0, len(eyes)-1)],
+                                arms[randint(0, len(arms)-1)])
 
 def ctrl_c(func):
     def wrap(*args, **kwargs):
@@ -18,17 +29,13 @@ def ctrl_c(func):
         return ret
     return wrap
 
-def printer(dancers, wait, clear=True):
-
-    dance_moves = [' `\_(^_^)`\_ ', ' _/`(^_^)_/` ', '  ~~(*_*)~~  ',
-            ' `\_(^_^)_/` ', ' _/`(-_^)`\_ ', ' _/`(^_-)`\_ ',
-            ' /.(@_@).\ ']
+def printer(wait, clear=True):
     if clear:
         system('cls' if name == 'nt' else 'clear')
         print('\n'*15)
     while True:
         print(
-            *[dance_moves[randint(0,6)] for i in range(dancers)],
+            *[move() for i in range(get_terminal_size()[0]//13)],
             end='\r', flush=True
             )
         sleep(wait)
@@ -36,7 +43,7 @@ def printer(dancers, wait, clear=True):
 @ctrl_c
 def main(clear=False):
     clear = bool(clear)
-    printer(5, .1, clear)
+    printer(.15, clear)
 
 if __name__ == "__main__":
     try:
