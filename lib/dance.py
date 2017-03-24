e9298e0cbf70aa999b5c5b4ea0b6dea6116c4da8
John Andersen <johnandersen185@gmail.com>
2017-03-24 18:32:10 -0400

    Updated dance.py. hooooo boy, it's going to be a long life

--- lib/dance.py
+++ lib/dance.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-from os import system, name, get_terminal_size
+
+from os import get_terminal_size
 from time import sleep
 from random import randint
 
+
 def move():
-    eyes = [ "^", "*", "@", "-", "#" ]
+    eyes = ["^", "*", "@", "-", "#"]
     arms = ["`\_", "_/`", "/.", "\.", "~~"]
     mouth = ["_", "o"]
     return "{: >4}({}{}{}){: <4}".format(
-                                arms[randint(0, len(arms)-1)],
-                                eyes[randint(0, len(eyes)-1)],
-                                mouth[randint(0, len(mouth)-1)],
-                                eyes[randint(0, len(eyes)-1)],
-                                arms[randint(0, len(arms)-1)])
+        arms[randint(0, len(arms) - 1)],
+        eyes[randint(0, len(eyes) - 1)],
+        mouth[randint(0, len(mouth) - 1)],
+        eyes[randint(0, len(eyes) - 1)],
+        arms[randint(0, len(arms) - 1)])
+
 
 # Wrapping void function so the return is irrelavant
 def ctrl_c(func):
@@ -20,21 +23,21 @@ def ctrl_c(func):
         try:
             func(*args, **kwargs)
         except KeyboardInterrupt:
-            print('\r\n\r'+ ' '*14 + '/' + '`'*6 + '\\')
+            print('\r\n\r' + ' ' * 14 + '/' + '`' * 6 + '\\')
             print('{}{{{: ^8}}}'.format(move(), "ByeBye"))
-            print(' '*14 + '\\' + '_'*6 +'/')
+            print(' ' * 14 + '\\' + '_' * 6 + '/')
             exit(0)
         return None
     return wrap
 
+
 @ctrl_c
 def main():
     while True:
-        print(
-            *[move() for i in range(get_terminal_size()[0]//13)],
-            end='\r', flush=True
-            )
+        print(*[move() for i in range(get_terminal_size()[0] // 13)],
+              end='\r', flush=True)
         sleep(.15)
+
 
 if __name__ == "__main__":
     main()
